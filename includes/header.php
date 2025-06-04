79c9e208a4b386a2ed4f928e538a01117c4e797e
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-06-04 16:28:20 +0200

    First commit

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<header>
+    <nav>
+        <a href="./index.php">
+            <h1 id="HeaderOne_H1">Mythica</h1>
+        </a>
+        <!-- Login and Register button -->
+        <div id="HeaderTwo_H2-LoginButtons">
+
+            <!-- Login Button -->
+            <a href="../pages/login.php">
+                <h2 class="ButtonH2">
+                    Login
+                </h2>
+            </a>
+
+
+            <!-- Register Button -->
+            <a href="#">
+                <h2 class="ButtonH2">
+                    Register
+                </h2>
+            </a>
+
+        </div>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<!-- 
+Auteur: Dewi
+Datum: 02-6-2025 
+Omschrijving: Praktijk Projectopdracht T04
+-->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mythica</title>
+    <link rel="stylesheet" href="./styles/stylesheet.css">
+    <link rel="stylesheet" href="./styles/home.css">
+
 </head>
 
 <body>
 
+    <?php
+    require "./includes/header.php"
+    ?>
+
+    <main>
+        <div id="MythicaPageOne">
+            <img id="MythicaPageOne-Image" alt="Different Creatures at the startpage" src="./images/Startpage-Creatures.png">
+
+            <h3>
+                Know everything about your favorite beast ,
+                <br><br>
+                <span class="H3-Cursive">Mythica, the encyclopedia</span>
+            </h3>
+        </div>
+    </main>
+
+    <?php
+    require "./includes/footer.php"
+    ?>
 </body>
 
 </html>
