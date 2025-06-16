bfe2c41322e3479796a00975723c07fb857b9e34
Dewi Zheng <dewi.zheng@edu-kw1c.nl>
2025-06-16 15:11:39 +0200

    Linking pages

--- includes/header.php
+++ includes/header.php
@@ -1,34 +1,31 @@
 <header>
     <nav>
-        <a href="../index.php">
+    <!-- Leest vanaf Mythica folder -->
+        <?php
+        $basePath = '/Mythica/'; // Set this to your project root folder or '/' if at root
+        ?>
+
+        <a href="<?php echo $basePath; ?>index.php">
             <h1 id="HeaderOne_H1">Mythica</h1>
         </a>
+
         <!-- Inlogknop en Overzicht met wezens knop -->
         <div id="HeaderTwo_H2-LoginButtons">
 
             <!-- Inloggen -->
-            <a href="../pages/login.php">
-                <h2 class="ButtonH2">
-                    Login
-                </h2>
+            <a href="<?php echo $basePath; ?>pages/login.php">
+                <h2 class="ButtonH2">Login</h2>
             </a>
-
 
             <!-- Registreren -->
-            <a href="../pages/register.php">
-                <h2 class="ButtonH2">
-                    Register
-                </h2>
+            <a href="<?php echo $basePath; ?>pages/register.php">
+                <h2 class="ButtonH2">Register</h2>
             </a>
-
 
             <!-- Overzicht met wezens -->
-            <a href="../pages/overzicht.php">
-                <h2 class="ButtonH2">
-                    Overzicht Wezens
-                </h2>
+            <a href="<?php echo $basePath; ?>pages/overzicht.php">
+                <h2 class="ButtonH2">Overzicht Wezens</h2>
             </a>
-
         </div>
     </nav>
 </header>
